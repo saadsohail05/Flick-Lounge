f56f4c63026383566d8dbc9348ddb18a63c84f65
saadsohail05 <saadsohail5104@gmail.com>
2024-04-12 16:39:05 +0500

    Routes Created

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+const express = require('express');
+const indexRoute = require('./routes/indexRoute');
+const userRoutes = require('./routes/userRoutes'); // Import userRoutes
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use indexRoute for the homepage
+app.use('/', indexRoute);
+
+// Use userRoutes for sign-in and sign-up
+app.use('/user', userRoutes); // Use '/user' as the base path for user routes
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
