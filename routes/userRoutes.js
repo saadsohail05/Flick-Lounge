eb104a79821614ceae9b5a5255e8929cdb7081e6
saadsohail05 <saadsohail5104@gmail.com>
2024-04-13 22:08:59 +0500

    Signin Script Fixed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,11 +7,11 @@ router.get('/signin', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/User/signin.html')); // Render the sign-in form
 });
 
-// Route for handling sign-in form submission
-router.post('/signin', (req, res) => {
-    // Process sign-in form submission
-    // Logic for authenticating the user
-});
+// // Route for handling sign-in form submission
+// router.post('/signin', (req, res) => {
+//     // Process sign-in form submission
+//     // Logic for authenticating the user
+// });
 
 // Route for displaying the sign-up form
 router.get('/signup', (req, res) => {
@@ -19,15 +19,13 @@ router.get('/signup', (req, res) => {
 });
 
 // Route for handling sign-up form submission
-router.post('/signup', (req, res) => {
-    // Process sign-up form submission
-    // Logic for creating a new user account
-});
+// router.post('/signup', (req, res) => {
+//     // Process sign-up form submission
+//     // Logic for creating a new user account
+// });
 router.get('/request-password-reset', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/User/forgot.html')); // Render the sign-in form
 });
 
-router.post('/signin', formController.sendData)
-router.post('/signup', formController.sendData)
 
 module.exports = router;
