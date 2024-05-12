980c8428f89c39b36bb5e27f6e642ac4908cd959
basilwarraich <basil451287@gmail.com>
2024-05-12 17:34:10 +0500

    search is working but is not fully implemented and content is not shown on page

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -2,23 +2,36 @@ const Movie = require('../models/movieScheme.model');
 
 exports.searchMovies = async (req, res) => {
     try {
-        const { keyword, type, genre, rating, page } = req.body;
+        const { keyword, genre, rating, year, page } = req.body;
         const pageSize = 10; // Number of movies per page
         const skip = (page - 1) * pageSize;
 
-        // Query MongoDB based on search criteria, pagination, and limit
-        const movies = await Movie.find({
-            name: { $regex: new RegExp(keyword, 'i') },
-            genre: genre !== 'All' ? genre : { $exists: true },
-            rating: { $gte: parseInt(rating) }
-        }).skip(skip).limit(pageSize);
+        console.log(keyword);
+        console.log(genre);
+        console.log(rating);
+        console.log(year);
+        // Construct the query object
+        let query = {};
+        if (keyword) query.name = keyword;
+        if (genre!='all') query.genre = genre;
+        if (rating!=0) query.rating = parseFloat(rating);
+        if (year!=0) query.year = year;
+
+        console.log(query);
+        // Execute the query with pagination
+        const movies = await Movie.find(query);
 
         res.json(movies);
+        if (movies.length === 0) {
+            console.log('No movies found.');
+        }
+
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
+
 
 exports.addMovie = async (req, res) => {
     try {
